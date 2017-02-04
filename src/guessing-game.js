cec6b78895f3d40382580b50962104d07e3ba2ef
Anton Glebov <workglebov@gmail.com>
2017-02-05 02:58:28 +0300

    guessing-game/implementation of the function

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = 0;
+        this.min = 0;
+    }
 
     setRange(min, max) {
-
+        this._min = min;
+        this._max = max;
     }
 
     guess() {
-
+        this._guesNum = Math.round((this._max + this._min) / 2);
+        return this._guesNum;
     }
 
     lower() {
-
+        this._max = this._guesNum;
     }
 
     greater() {
-
+        this._min = this._guesNum;
     }
 }
 
